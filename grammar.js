02781fd743ca531e4f927e870a01c3b0e82d243f
Leon Fuss <leon.fuss@icloud.com>
2024-07-26 17:34:04 +0200

    syntax for type and record

--- grammar.js
+++ grammar.js
@@ -1,6 +1,22 @@
 module.exports = grammar({
   name: "effekt",
 
+  keyword: ($) =>
+    choice(
+      "record",
+      "type",
+      "interface",
+      "def",
+      "if",
+      "else",
+      "match",
+      "case",
+      "with",
+      "do",
+    ),
+
+  precedences: (_) => [["multiplication", "addition"]],
+
   rules: {
     source_file: ($) => repeat($._definition),
 
@@ -9,21 +25,78 @@ module.exports = grammar({
     line_comment: ($) => token(seq("//", /.*/)),
 
     record: ($) =>
-      seq("record", field("name", $.ident), "(", repeat($.record_field), ")"),
+      seq(
+        "record",
+        field("name", $.type_identifier),
+        optional(field("parameters", $.type_parameters)),
+        "(",
+        optional(field("members", $.parameters)),
+        ")",
+      ),
 
-    record_field: ($) => seq($.ident, ":", $._type_ident, optional(",")),
+    // Record the type parameters
+    type_parameters: ($) => seq("[", commaSep1($.generic_identifier), "]"),
+
+    // Define parameter list with possible trailing comma
+    parameters: ($) => trailingCommaSep1($.parameter),
+
+    // Define a parameter
+    parameter: ($) =>
+      seq(field("name", $.identifier), ":", field("kind", $.parameter_type)),
+
+    // Define parameter type which may include generic arguments
+    parameter_type: ($) =>
+      seq(
+        $.type_identifier,
+        optional(seq("[", commaSep1($.generic_identifier), "]")),
+      ),
 
     type: ($) =>
-      seq("type", field("name", $.ident), "{", repeat($.type_field), "}"),
+      seq(
+        "type",
+        field("name", $.type_identifier),
+        optional(field("parameters", $.type_parameters)),
+        "{",
+        optional(field("members", $.type_members)),
+        "}",
+      ),
 
-    type_field: ($) => seq($.ident, "(", ")", optional(";")),
+    type_members: ($) => semilicolonSep1($.type_member),
 
-    ty_ident: ($) => seq($.ident, ":", $._type_ident),
+    type_member: ($) =>
+      seq(field("member", $.type_identifier), "(", optional($.parameters), ")"),
 
-    _type_ident: ($) => seq($.ident, optional(seq("[", $.generic_ident, "]"))),
-
-    ident: ($) => /[a-zA-Z][0-9a-zA-Z_]+/,
-
-    generic_ident: ($) => /[a-zA-Z]+/,
+    // Defining identifiers for type names and variable names
+    generic_identifier: ($) => /[A-Z][a-zA-Z0-9_]*/,
+    type_identifier: ($) => /[A-Z][a-zA-Z0-9_]*/,
+    identifier: ($) => /[a-z][a-zA-Z0-9_]*/,
   },
 });
+
+function commaSep(rule) {
+  return optional(commaSep1(rule));
+}
+
+function commaSep1(rule) {
+  return sep1(",", rule);
+}
+
+function trailingCommaSep(rule) {
+  return optional(trailingCommaSep1(rule));
+}
+
+function trailingCommaSep1(rule) {
+  return trailingSep1(",", rule);
+}
+
+function semilicolonSep1(rule) {
+  return sep1(";", rule);
+}
+
+function trailingSep1(delimiter, rule) {
+  return seq(sep1(delimiter, rule), optional(delimiter));
+}
+
+function sep1(delimiter, rule) {
+  return seq(rule, repeat(seq(delimiter, rule)));
+}
